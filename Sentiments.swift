a7f1528c53f408657b01c6ad47e3d82bd6da137b
Carmelo <carmelo.arancio19@hotmail.com>
2022-10-18 14:47:29 +0200

    Sentiments
    Created Enum for sentiments sad and Happy

---
+++ Sentiments.swift
@@ -0,0 +1,21 @@
+//
+//  Sentiments.swift
+//  MyBff
+//
+//  Created by Carme on 18/10/2022.
+//
+
+import Foundation
+
+enum Sentiments {
+    case happy
+    case sad
+    
+    var imageAI: UIImage {
+        switch self {
+        case .happy:
+                return UIImage(named: "happy")
+        case .sad:
+            return UIImage(named: "sad")
+    }
+}
